4dc72533ef0b5972a71b742cb37b3459f6220d37
Vishnu Kumar <148890416+Vishnu-android@users.noreply.github.com>
2025-11-23 18:09:44 +0530

    Fix: Search input debouncing fixed

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { HomeIcon, UploadIcon, BarChartIcon, LogOutIcon, UserIcon, PlayCircleIcon, ListIcon,HistoryIcon } from './Icons';
 
-// Add History Icon
-const HistoryIcon = ({ className = "w-5 h-5" }) => (
-  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} 
-      d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-);
+
 
 const Navbar = ({ user, onLogout, activeTab, setActiveTab }) => (
   <nav className="navbar">
